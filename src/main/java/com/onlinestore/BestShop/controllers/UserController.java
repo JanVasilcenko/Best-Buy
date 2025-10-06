63b671f99a839003f8b164ae5759700045f8329d
JanVasilcenko <62397372+JanVasilcenko@users.noreply.github.com>
2025-10-06 16:08:43 +0200

    Added Swagger UI documentation of REST endpoints

--- src/main/java/com/onlinestore/BestShop/controllers/UserController.java
+++ src/main/java/com/onlinestore/BestShop/controllers/UserController.java
@@ -4,16 +4,18 @@ import com.onlinestore.BestShop.model.User;
 import com.onlinestore.BestShop.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
+@RequestMapping("/users")
 public class UserController {
     private final UserService userService;
 
-    @GetMapping("/users")
+    @GetMapping()
     public ResponseEntity<?> GetUserById(@RequestParam(name = "userID") String userID){
         User user = userService.getUserByID(userID);
 
