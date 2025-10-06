630ff9a80e262525eb8b7a99c576162e615d87b0
JanVasilcenko <62397372+JanVasilcenko@users.noreply.github.com>
2025-10-06 12:02:47 +0200

    Initial_Commit

---
+++ src/main/java/com/onlinestore/BestShop/BestShopApplication.java
@@ -0,0 +1,13 @@
+package com.onlinestore.BestShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestShopApplication.class, args);
+	}
+
+}
