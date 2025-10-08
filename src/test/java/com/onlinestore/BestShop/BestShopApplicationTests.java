c86cc8bfdbd221d9ce5229c4f6bb34a199ca5d21
JanVasilcenko <62397372+JanVasilcenko@users.noreply.github.com>
2025-10-08 16:52:38 +0200

    Remove unused imports

--- src/test/java/com/onlinestore/BestShop/BestShopApplicationTests.java
+++ src/test/java/com/onlinestore/BestShop/BestShopApplicationTests.java
@@ -1,14 +1,8 @@
 package com.onlinestore.BestShop;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 @ActiveProfiles("test")
 @SpringBootTest
