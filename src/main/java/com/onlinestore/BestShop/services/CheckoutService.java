873fbb38bb7491f316448fee6ee8a862227f2618
JanVasilcenko <62397372+JanVasilcenko@users.noreply.github.com>
2025-10-16 12:57:12 +0200

    Fix for secret key

--- src/main/java/com/onlinestore/BestShop/services/CheckoutService.java
+++ src/main/java/com/onlinestore/BestShop/services/CheckoutService.java
@@ -12,8 +12,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.Instant;
-
 @Service
 @RequiredArgsConstructor
 public class CheckoutService {
